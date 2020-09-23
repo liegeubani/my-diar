214fc8dbab1503ba44e3df5e7299d0798cef80e5
Jelili Noibi <drealjstar@yahoo.com>
2020-09-23 22:36:37 +0100

    create home page design

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
-import './../Css/Home.css';
+import React, { Component } from 'react';
+import './../Css/Home.css' ;
+import myImg from "./../diaryPic2.jpg"
 
 
-
-const Home = () => <h2>Home</h2>
+class Home extends Component{
+    render(){
+        return(
+            <div className= "body">
+            <div className= "pic">
+                <img src ={myImg} alt= "n" width= "1290" height= "425" />
+            </div>
+           <h1>My Diary</h1>
+          <p>Pen down your thought on the go ...</p>
+          <button>Get Started</button>
+                
+            </div>
+        )
+    }
+}
 
 export default Home;
