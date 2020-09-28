05c09c203fa84897379fbd05f308be2bb69dede9
Liege <40875240+liegeubani@users.noreply.github.com>
2020-09-28 20:06:46 +0100

    updated the login page to the latest login page on the figma

--- src/Components/login.jsx
+++ src/Components/login.jsx
@@ -4,13 +4,13 @@ import '../Css/login.css'
 class Login extends React.Component {
     state = {
         userName: '',
-        userPassword: null,
+        userPassword: '',
         user: []
 
     }
 
     handleUsername = (e) => {
-        this.setState({ userName: e.target.value.toUpperCase() })
+        this.setState({ userName: e.target.value })
     }
 
     handleUserpassword = (e) => {
@@ -18,27 +18,23 @@ class Login extends React.Component {
     }
     onSubmit = (e) => {
         e.preventDefault()
-        const { userName, userPassword, userUpdate } = this.state
-        if (userName !== '' && userPassword !== '') {
-            this.setState({ userUpdate: [userUpdate, userName, userPassword] })
-        }
+        this.setState({ userUpdate: this.state.user })
     }
     render() {
         const { userName, userPassword } = this.state
-
+        console.log(this.state)
         return (
             <div className="main-container" >
-                 
+                <header className="formHeader">My Dairy</header>
                 <form onSubmit={this.onSubmit}>
-                    <caption className="header-text">Login</caption>
+                    <header className="header-text">Login</header>
                     <input type="text" value={userName} onChange={this.handleUsername} placeholder='user-name' />
                     <input type="password" value={userPassword} onChange={this.handleUserpassword} placeholder='password' />
                     <input type="submit" value="Login" />
                 </form>
                 <div className="footer">
-                    <h5 className="forgotpassword"><span>forgot</span> <a href="/">password?</a></h5>
-                    <h5 className="signup"><span>create account?</span> <a href="/">Sign Up</a></h5>
-                   </div>
+                    <h5 className="signup">Don`t have an account?<a href="/">Sign Up</a></h5>
+                </div>
             </div>
         )
     };
