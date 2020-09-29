750a8556adaf37d180ecd87a7e4966bb7d11543e
Jelili Noibi <drealjstar@yahoo.com>
2020-09-29 17:48:15 +0100

    modify home page to follow  new design

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,22 +1,26 @@
-import React, { Component } from 'react';
-import './../Css/Home.css' ;
-import myImg from "./../diaryPic2.jpg"
+import React, { Component } from "react";
+import "./../Css/Home.css";
 
-
-class Home extends Component{
-    render(){
-        return(
-            <div className= "body">
-            <div className= "pic">
-                <img src ={myImg} alt= "n" width= "1290" height= "425" />
-            </div>
-           <h1>My Diary</h1>
-          <p>Pen down your thought on the go ...</p>
-          <button>Get Started</button>
-                
-            </div>
-        )
-    }
+class Home extends Component {
+  render() {
+    return (
+      <div className="home-container">
+        <div class="topic">
+          <h1 className="app-name">My Diary</h1>
+          <p className="app-discription">Online Journal</p>
+          <p className="record">
+            Whether you're looking for a tool to record your daily emotions....
+          </p>
+        </div>
+        <div className="signinBtnWrapper">
+          <button className="home-button">Signin</button>
+          <p className="account">
+            Don't have an account? <span className="home-signup">Signup</span>
+          </p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Home;
